1b54d39328124b8e5b7f070e3f97b0f07502fbce
eurochriskelly@gmail.com <eurochriskelly@gmail.com>
2023-09-26 07:20:15 +0200

    Implement update script

---
+++ apps/bash/bash_include.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO: merge bash scripts
+
+alias ll=`ls -alh'
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# Run from top-level using:
+#
+# `bash scripts/update.sh`
+#
+set -e
 
+# update Xresources
+cp config/Xresources ~/.Xresources
+cd ~
 xrdb -merge .Xresources
+cd -
+
+# update emacs config
+test -d ~/.emacs.d || mkdir .emacs.d
+test -f ~/.emacs && mv .emacs .emacs.$(date +%s)
+cp -r apps/emacs/* .emacs.d/
+
+# update tmux
+cp apps/tmux/tmux.conf ~/.tmux.conf
+
 
