88651928f807c33984d0fa57be44e62cc34b018a
eurochriskelly@gmail.com <eurochriskelly@gmail.com>
2023-12-03 22:50:52 +0100

    Better filtering of visibility of future tasks

--- apps/emacs/my-org.el
+++ apps/emacs/my-org.el
@@ -1,5 +1,19 @@
 ;; my-org.el
 (require 'org)
+
+;; Custom skip function to handle future scheduled tasks
+(defun my-skip-unless-ready ()
+  "Skip entries that are not ready to be shown."
+  (let* ((scheduled-time (org-get-scheduled-time (point)))
+	 (scheduled-day (and scheduled-time (time-to-days scheduled-time)))
+	 (current-day (time-to-days (current-time)))
+	 (subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (or (not scheduled-day)  ;; If there's no scheduled day, don't skip
+	    (<= scheduled-day current-day))
+	nil  ;; Do not skip this entry
+      subtree-end)
+    ))
+
 ;; Set up the default key-bindings
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-cc" 'org-capture)
@@ -8,17 +22,24 @@
 
 (message "set keywords")
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "NEXT(n)" "PROGRESS(p)" "DONE(d)" "CANCELLED(c)")))
+      '((sequence "TODO(t)" "NEXT(n)" "PROGRESS(p)" "DONE(d)" "CANCELLED(c)" "DELEGATED(l)")))
 
 (message "Setup agenda commands")
 (setq org-agenda-custom-commands
-      '(("o n" "Next and Progress Tasks" todo "NEXT|PROGRESS")
-	("o p" "Progress Tasks" todo "PROGRESS")))
+      '(("o n" "Next and Progress Tasks"
+	 todo "NEXT|PROGRESS"
+	 ((org-agenda-skip-function 'my-skip-unless-ready)))
+	("o p" "Progress Tasks"
+	 todo "PROGRESS")
+	("o t" "Todo without future tasks"
+	 todo "TODO"
+	 ((org-agenda-skip-function 'my-skip-unless-ready)))))
 
 ;; Binding C-c o n to the custom agenda view
 (global-set-key (kbd "C-c o n") (lambda () (interactive) (org-agenda nil "o n")))
 
-
+;; Binding C-c o t for Todo items without future tasks
+(global-set-key (kbd "C-c o t") (lambda () (interactive) (org-agenda nil "o t")))
 
 (message "Set org agenda files")
 (setq org-agenda-files
